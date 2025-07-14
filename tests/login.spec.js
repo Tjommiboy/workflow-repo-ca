4c3fbb236e687594a13da50c90ea52b1f8fc3482
Tjommiboy <119463874+Tjommiboy@users.noreply.github.com>
2025-07-14 15:30:11 +0700

    configured playwright and vitest compatability, added login user test

---
+++ tests/login.spec.js
@@ -0,0 +1,16 @@
+import { test, expect } from "@playwright/test";
+
+const username = process.env.TEST_USERNAME;
+const password = process.env.TEST_PASSWORD;
+
+test.describe("login", () => {
+  test("User can login", async ({ page }) => {
+    await page.goto("http://127.0.0.1:5500/login/");
+
+    await page.locator('input[name="email"]').fill(username);
+    await page.locator('input[name="password"]').fill(password);
+
+    await page.getByRole("button", { name: "Login" }).click();
+    await expect(page.getByRole("button", { name: "Logout" })).toBeVisible();
+  });
+});
