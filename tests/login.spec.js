490900dffe67cf44e0d25e39289a8e70b522320e
Tjommiboy <119463874+Tjommiboy@users.noreply.github.com>
2025-07-14 15:38:35 +0700

    adds test:User sees an error message with invalid credentials

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -13,4 +13,16 @@ test.describe("login", () => {
     await page.getByRole("button", { name: "Login" }).click();
     await expect(page.getByRole("button", { name: "Logout" })).toBeVisible();
   });
+  test("shows error message with invalid credentials", async ({ page }) => {
+    await page.goto("http://127.0.0.1:5500/login/");
+
+    await page.locator('input[name="email"]').fill(username);
+    await page.locator('input[name="password"]').fill("wrongpassword");
+
+    await page.getByRole("button", { name: "Login" }).click();
+
+    await expect(page.locator("#message-container")).toContainText(
+      "Invalid email or password",
+    );
+  });
 });
